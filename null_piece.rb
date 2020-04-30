107624d3bb699a5f9650bf5d7149afbeec52a73c
Arthur Yao <yakunyao@gmail.com>
2020-04-30 09:03:58 +0800

    Add NullPiece

--- null_piece.rb
+++ null_piece.rb
@@ -1,1 +1,12 @@
-require_relative 'piece'
+require 'singleton'
+
+class NullPiece
+    
+    inlcude Singleton
+    attr_reader :color, :symbol
+
+    def initialize
+        @color = nil
+        @symbol = :NP
+    end
+end
